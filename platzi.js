223230421a9b3410baa9568069396cd1847b41e3
GuadalupFranco <franco.ramirez.guadalup@gmail.com>
2022-06-23 15:04:14 -0500

    implementation of proxy

--- platzi.js
+++ platzi.js
@@ -1,16 +1,27 @@
 class PlatziReactive {
     constructor(options) {
-        this.origen = options.data();
+        this.origin = options.data();
+
+        // Destiny
+        this.$data = new Proxy(this.origin, {
+            get(target, name) {
+                if (name in target) {
+                    return target[name];
+                }
+                console.warn("The property", name, "doesn't exists")
+                return "";
+            }
+        })
     }
 
     mount(){
         document.querySelectorAll("*[p-text]").forEach(el => {
-            this.pText(el, this.origen, el.getAttribute("p-text"))
+            this.pText(el, this.$data, el.getAttribute("p-text"))
         })
     }
 
     pText(el, target, name) {
-        el.innerText = target   [name];
+        el.innerText = target[name];
     }
 
     pModel(){}
