f4d70497228593b6390ad62ecf969d7dc49eb757
GuadalupFranco <franco.ramirez.guadalup@gmail.com>
2022-06-26 16:45:53 -0500

    create product component in option api

---
+++ options/Product.js
@@ -0,0 +1,84 @@
+app.component("product", {
+
+    template: /* vue-html */ `
+    <section class="product">
+        <div class="product__thumbnails">
+          <div 
+            v-for="(image, index) in product.images"
+            :key="image.thumbnail"
+            class="thumb" 
+            :class="{active: activeImage == index}"
+            :style="{ backgroundImage : 'url('+ image.thumbnail +')'}"
+            @click="activeImage = index"></div>
+        </div>
+        <div class="product__image">
+          <img :src="product.images[activeImage].image" :alt="product.name">
+        </div>
+      </section>
+      <section class="description">
+        <h4>{{ product.name.toUpperCase() }} {{ product.stock === 0 ? ":(" : ":)"}}</h4>
+        <span class="badge new" v-if="product.new">New</span>
+        <span class="badge offer" v-if="product.offer">Offer</span>
+        <p class="description__status" v-if="product.stock <= 4 && product.stock > 1">There are a few units left</p>
+        <p class="description__status" v-else-if="product.stock == 1">Just one unit</p>
+        <p class="description__status" v-else-if="product.stock == 0">Not available</p>
+        <p class="description__price">
+            $ {{ new Intl.NumberFormat("es-MX").format(product.price) }}
+        </p>
+        <p class="description__content"> </p>
+        <div class="discount">
+          <span>Cuppon</span>
+          <input 
+            type="text" 
+            placeholder="Enter your code" 
+            @keyup.enter="applyDiscount($event)">
+        </div>
+        <button :disabled="product.stock == 0" @click="addToCart()">Add to cart</button>
+      </section>
+    `,
+    data() {
+        return {
+            product: {
+                name: "Camera",
+                price: 2_500,
+                stock: 5,
+                content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet delectus, cum
+                  blanditiis eos quas illo eaque aperiam harum quae soluta ducimus distinctio doloribus est sunt, maiores
+                  molestiae natus recusandae praesentium.`,
+                images: [
+                    {
+                        image: "./images/camera.jpg",
+                        thumbnail: "./images/camera-thumb.jpg"
+                    },
+                    {
+                        image: "./images/camera-2.jpg",
+                        thumbnail: "./images/camera-2-thumb.jpg"
+                    }
+                ],
+                new: false,
+                offer: false,
+                quantity: 1,
+            },
+            activeImage: 0,
+            discountCodes: ["VUE20", "FRNCO"],
+        }
+    },
+    methods: {
+        applyDiscount(event) {
+            const discountCodeIndex = this.discountCodes.indexOf(event.target.value);
+            if (discountCodeIndex >= 0) {
+                this.product.price *= 0.5;
+                this.discountCodes.splice(discountCodeIndex, 1);
+            }
+        },
+        addToCart() {
+            const prodIndex = this.cart.findIndex(prod => prod.name == this.product.name);
+            if (prodIndex >= 0) {
+                this.cart[prodIndex].quantity += 1;
+            } else {
+                this.cart.push(this.product);
+            }
+            this.product.stock -= 1;
+        }
+    }
+})
